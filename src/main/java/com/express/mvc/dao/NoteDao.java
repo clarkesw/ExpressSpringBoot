9a2153e79f02dff559e1efe8a9093ca67e493eb9
clarkesw <clarke.job@gmail.com>
2024-08-08 11:30:49 -0400

    Fix for NoteDao part 3

--- src/main/java/com/express/mvc/dao/NoteDao.java
+++ src/main/java/com/express/mvc/dao/NoteDao.java
@@ -4,8 +4,7 @@ import com.express.mvc.model.Note;
 import java.util.List;
 
 
-public interface NoteDAO {
-
-	public void save(Note note);
-	public List<Note> findAll();
+public interface NoteDao {
+    public void save(Note note);
+    public List<Note> findAll();    
 }
